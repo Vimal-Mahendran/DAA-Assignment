7c68d6cc28836874e1c30ee5c42565136ed5c56f
Vimal-Mahendran <151828616+Vimal-Mahendran@users.noreply.github.com>
2024-06-12 13:01:22 +0530

    10.Count Subarrays With Median K.py

--- Assignment-6/10.Count Subarrays With Median K.py
+++ Assignment-6/10.Count Subarrays With Median K.py
@@ -9,12 +9,6 @@ def count_subarrays_with_median_k(nums, k):
                 count += 1
     return count
 
-# Example 1
 nums1 = [3, 2, 1, 4, 5]
 k1 = 4
-print(count_subarrays_with_median_k(nums1, k1))  # Output: 3
-
-# Example 2
-nums2 = [2, 3, 1]
-k2 = 3
-print(count_subarrays_with_median_k(nums2, k2))  # Output: 1
+print(count_subarrays_with_median_k(nums1, k1))
